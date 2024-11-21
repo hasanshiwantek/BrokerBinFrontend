e92a059d660f9052fb8dea1ac3da73a839f2dcce
FaizanShamsi <faizanshamsi2017@gmail.com>
2024-11-21 20:26:33 +0500

    viteconfig file updated

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Set base path
-  server: {
-    historyApiFallback: true, // Handle client-side routing
-  },
-  build: {
-    outDir: "dist", // Ensure Vercel output directory
-    sourcemap: true, // Debugging support
-  },
 })
