e1ab9c88d6c9fee1c8f72674940218d1d94b4c74
hasanshiwantek <hasan.shiwantek@gmail.com>
2024-12-11 23:50:00 +0500

    Updated h

--- src/components/api/BrokerEndpoint.js
+++ src/components/api/BrokerEndpoint.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // export const brokerAPI="https://brokerbinbackend.shiwantek.com/api/"
-=======
-export const brokerAPI="https://brokerbinbackend.shiwantek.com/api/"
->>>>>>> f84b24171e628463c8568e4a666e75c6de20a4dd
 
 
 export const brokerAPI="https://brokerbinbackend.shiwantek.com/api/"
