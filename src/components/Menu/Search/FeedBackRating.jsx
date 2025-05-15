454f1c7d2a1f7a2460bdaf78fe9df9784e77474c
Faizan <faizanshamsi2017@gmail.com>
2025-05-15 23:37:13 +0500

    minor fixes

--- src/components/Menu/Search/FeedBackRating.jsx
+++ src/components/Menu/Search/FeedBackRating.jsx
@@ -39,9 +39,6 @@ const FeedBackRating = () => {
     console.log(primaryId);
     dispatch(fetchUserData({ id:primaryId, token }));
   }, []);
-
-
-
   console.log("Initial Data ", initialData);
 
   useEffect(() => {
