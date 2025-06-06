cc30802e6b00410c11b51dc3942a2e8bb1c3151c
Faizan <faizanshamsi2017@gmail.com>
2025-06-06 22:39:48 +0500

    minor fix

--- src/components/Popups/CompanyDetails/CompanyDetails.jsx
+++ src/components/Popups/CompanyDetails/CompanyDetails.jsx
@@ -75,10 +75,9 @@ const CompanyDetails = ({ closeModal }) => {
   console.log("COMPANY CONTACT DATA FROM FRONTEND", companyContactData);
 
   // Handle empty company array or error
-  // if (!company || !companyId) {
-  //   return <h2>No company data available.</h2>; 
-
-  // }
+  if (!company || !companyId) {
+    return <h2>No company data available.</h2>; 
+  }
 
   useEffect(() => {
     if (companyId && token) {
